4a3f469a7d58e6fcad920d8552b4dd76d6d31ffe
florent <florent.le.pape@gmail.com>
2024-10-28 09:03:27 +0100

    keep only frontend + put front at root

--- backend/src/auth/constants.ts
+++
@@ -1,5 +0,0 @@
-import * as bcrypt from 'bcrypt';
-
-export const jwtConstants = {
-	secret : process.env.DEV_JWT_SECRET || 'secretKey'
-}
--- backend/src/habit-completions/dto/update-habit-completion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateHabitCompletionDto } from './create-habit-completion.dto';
-
-export class UpdateHabitCompletionDto extends PartialType(CreateHabitCompletionDto) {}
